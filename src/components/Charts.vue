211a649f08d097f03ac53ece5bd4b6047174daef
danielkasperczyk <danielkasperczyk@gmail.com>
2021-03-01 20:10:40 +0100

    removed inline styles

--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -1,19 +1,19 @@
 <template>
-  <v-container style="height: 500px">
+  <v-container>
     <v-layout row>
       <v-flex xs12 md6>
-        <v-btn outline block class="primary">1</v-btn>
+        <v-btn block class="primary">1</v-btn>
       </v-flex>
       <v-flex xs12 md6>
-        <v-btn outline block class="primary">2</v-btn>
+        <v-btn block class="primary">2</v-btn>
       </v-flex>
     </v-layout>
     <v-layout row>
       <v-flex xs12 md6>
-        <v-btn outline block class="primary">3</v-btn>
+        <v-btn block class="primary">3</v-btn>
       </v-flex>
       <v-flex xs12 md6>
-        <v-btn outline block class="primary">4</v-btn>
+        <v-btn block class="primary">4</v-btn>
       </v-flex>
     </v-layout>
   </v-container>
