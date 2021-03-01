d77ab09fc418864f9e951e14d66f9b1b8bc4e2bb
danielkasperczyk <danielkasperczyk@gmail.com>
2021-03-01 20:09:26 +0100

    aded vuex to project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
+import store from './store';
 import vuetify from './plugins/vuetify';
 import 'leaflet/dist/leaflet.css';
 
@@ -7,5 +8,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
+  store,
   render: (h) => h(App),
 }).$mount('#app');
