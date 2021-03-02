739cbf6936388fee72fb1cb61fb8a7fd3ba1d44f
Kasperczyk Daniel <d.kasperczyk@7technology.pl>
2021-03-02 09:52:05 +0100

    aded markers on map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,11 +1,23 @@
 <template>
   <l-map style="height: 60vh; width: 100%" :zoom="zoom" :center="center">
     <l-tile-layer :url="url"></l-tile-layer>
+    <l-marker
+      v-for="(data, index) in getCountryData"
+      :lat-lng="data.latLng"
+      :key="index"
+    >
+      <l-popup class="text-center">
+        {{ data.country }}
+        <br />
+        Cases: {{ data.cases }}
+      </l-popup>
+    </l-marker>
   </l-map>
 </template>
 
 <script>
-import { LMap, LTileLayer } from 'vue2-leaflet';
+import { mapGetters } from 'vuex';
+import { LMap, LTileLayer, LMarker, LPopup } from 'vue2-leaflet';
 
 export default {
   name: 'vue',
@@ -13,6 +25,8 @@ export default {
   components: {
     LMap,
     LTileLayer,
+    LMarker,
+    LPopup,
   },
   data() {
     return {
@@ -20,6 +34,9 @@ export default {
       zoom: 8,
       center: [47.31322, -1.319482],
     };
+  },
+  computed: {
+    ...mapGetters('data', ['getCountryData']),
   },
 };
 </script>
