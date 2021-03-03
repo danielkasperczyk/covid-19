1a155fb03a3b3eff07eec774fa532bce4f3f88b8
Kasperczyk Daniel <d.kasperczyk@7technology.pl>
2021-03-03 13:05:47 +0100

    added button to fetch Global data

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,18 @@
 <template>
-  <div>
-    <div class="d-flex align-items-center" width="100">
-      <h2>COVID-19 Tracker</h2>
-    </div>
-  </div>
+  <v-container class="d-flex align-items-center justify-space-between">
+    <h2>COVID-19 Tracker</h2>
+    <v-btn outlined @click="fetchCountryData('')"> Global Data </v-btn>
+  </v-container>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   name: 'Header',
+  methods: {
+    ...mapActions('data', ['fetchCountryData']),
+  },
 };
 </script>
 
