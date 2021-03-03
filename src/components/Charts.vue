1bd09e988898e86cbc18b6934a916febc23b7940
Kasperczyk Daniel <d.kasperczyk@7technology.pl>
2021-03-03 13:05:25 +0100

    displaying data about selected country or global

--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -1,27 +1,56 @@
 <template>
   <v-container>
-    <v-layout row>
-      <v-flex xs12 md6>
-        <v-btn block class="primary">1</v-btn>
-      </v-flex>
-      <v-flex xs12 md6>
-        <v-btn block class="primary">2</v-btn>
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-flex xs12 md6>
-        <v-btn block class="primary">3</v-btn>
-      </v-flex>
-      <v-flex xs12 md6>
-        <v-btn block class="primary">4</v-btn>
-      </v-flex>
-    </v-layout>
+    <div v-show="selectedCountry.country" class="text-center">
+      <h2>{{ selectedCountry.country }}</h2>
+    </div>
+    <v-row no-gutters class="d-flex justify-center">
+      <v-col cols="12" sm="4">
+        <v-card class="pa-2 text-center" outlined tile>
+          <h3>Cases</h3>
+          <p class="ma-0">
+            {{ selectedCountry.cases }}
+          </p>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="4">
+        <v-card class="pa-2 text-center" outlined tile>
+          <h3>Active</h3>
+          <p class="ma-0">
+            {{ selectedCountry.active }}
+          </p>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row no-gutters class="d-flex justify-center">
+      <v-col cols="12" sm="4">
+        <v-card class="pa-2 text-center" outlined tile>
+          <h3>Death</h3>
+          <p class="ma-0">
+            {{ selectedCountry.deaths }}
+          </p>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="4">
+        <v-card class="pa-2 text-center" outlined tile>
+          <h3>Recovered</h3>
+          <p class="ma-0">
+            {{ selectedCountry.recovered }}
+          </p>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import { mapGetters, mapState } from 'vuex';
+
 export default {
   name: 'Charts',
+  computed: {
+    ...mapState('data', ['selectedCountry']),
+    ...mapGetters('data', ['getGlobalData']),
+  },
 };
 </script>
 
