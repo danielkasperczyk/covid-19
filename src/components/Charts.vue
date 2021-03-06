12195f1dc258211ad93e6e4225032e198b74c1d7
danielkasperczyk <75161634+danielkasperczyk@users.noreply.github.com>
2021-03-06 16:55:29 +0100

    fetching data from vuex not axios

--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -1,14 +1,16 @@
 <template>
-  <v-container>
-    <div v-show="selectedCountry.country" class="text-center">
-      <h2>{{ selectedCountry.country }}</h2>
+  <v-container v-if="selectedCountry.cases || selectedCountry.country">
+    <div class="text-center">
+      <h2>
+        {{ selectedCountry.country ? selectedCountry.country : 'Global' }}
+      </h2>
     </div>
     <v-row no-gutters class="d-flex justify-center">
       <v-col cols="12" sm="4">
         <v-card class="pa-2 text-center" outlined tile>
           <h3>Cases</h3>
           <p class="ma-0">
-            {{ selectedCountry.cases }}
+            {{ numberWithCommas(selectedCountry.cases) }}
           </p>
         </v-card>
       </v-col>
@@ -16,7 +18,7 @@
         <v-card class="pa-2 text-center" outlined tile>
           <h3>Active</h3>
           <p class="ma-0">
-            {{ selectedCountry.active }}
+            {{ numberWithCommas(selectedCountry.active) }}
           </p>
         </v-card>
       </v-col>
@@ -26,7 +28,7 @@
         <v-card class="pa-2 text-center" outlined tile>
           <h3>Death</h3>
           <p class="ma-0">
-            {{ selectedCountry.deaths }}
+            {{ numberWithCommas(selectedCountry.deaths) }}
           </p>
         </v-card>
       </v-col>
@@ -34,7 +36,7 @@
         <v-card class="pa-2 text-center" outlined tile>
           <h3>Recovered</h3>
           <p class="ma-0">
-            {{ selectedCountry.recovered }}
+            {{ numberWithCommas(selectedCountry.recovered) }}
           </p>
         </v-card>
       </v-col>
@@ -45,11 +47,17 @@
 <script>
 import { mapGetters, mapState } from 'vuex';
 
+import numberWithCommas from '@/units/units';
+
 export default {
   name: 'Charts',
+
   computed: {
     ...mapState('data', ['selectedCountry']),
     ...mapGetters('data', ['getGlobalData']),
+  },
+  methods: {
+    numberWithCommas,
   },
 };
 </script>
